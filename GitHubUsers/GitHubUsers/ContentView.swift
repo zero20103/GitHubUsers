f9dc983421206ac3154c49b55b099e00a6d8851f
Sean Chen <zero20103@gmail.com>
2020-02-22 09:02:40 +0800

    add project

---
+++ GitHubUsers/GitHubUsers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubUsers
+//
+//  Created by Sean Chen on 2020/2/22.
+//  Copyright © 2020 Sean Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
